f2783151df01632096dfb730e2338bf86037a4db
MakamuT <makamuthe2@gmail.com>
2025-06-17 01:27:41 +0200

    Add initial string manipulation functions in strings.py

---
+++ Python/strings.py
@@ -0,0 +1,9 @@
+#strings
+
+message = " Hello, World "
+
+print(message.strip())#remove whitespaces
+print(message.lower())#convert characters to lower case
+print(message.split(','))#split a string into a list based on the comma
+print(message.upper())#convert characters to upper case
+print(message.replace())
