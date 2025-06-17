8d55dc8f07fc44efdb3d633bb9d02fa6516702af
MakamuT <makamuthe2@gmail.com>
2025-06-17 16:27:55 +0200

    Add control statements example to demonstrate conditional logic in Python

--- Python/numericData.py
+++
@@ -1,6 +0,0 @@
-#numeric data
-
-num = 3
-print(type(num))
-num2 = 3.14
-print(type(num2))
---
+++ Python/python.py
@@ -0,0 +1,10 @@
+#control statements
+
+num = -5
+
+if num > 0:
+    print("This number is positive")
+elif num == 0:
+    print("This number is 0")
+else:
+    print("This negative")
